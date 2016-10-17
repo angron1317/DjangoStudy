21e8e16ead1c7d66acacfe21558ed8bb6fec7643
angron1317 <angron1317@outlook.com>
2016-10-18 04:12:48 +0800

    修改文件，完成了7.3.4。

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def article_list(request,block_id):
+    block_id=int(block_id)
+    return render(request,'article_list.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^article/',include('article.urls')),
     #url(r'^static/(?P<path>.*)$',django.contrib.staticfiles.views.serve),
     #Django1.10不再需要
     url(r'^$',views.index),
